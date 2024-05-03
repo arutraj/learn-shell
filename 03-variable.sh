358ced3b85583397edb5609109c1d803b6ea6b9d
SVR Kalluru <raghuk.vit@gmail.com>
2024-05-03 08:54:04 +0530

    Variables demo

---
+++ 03-variable.sh
@@ -0,0 +1,11 @@
+# If we hold some data with some name then it is variable.
+# if we assign a name to a set of data then it is called as a variable.
+
+# syntax: var=data
+x=10
+
+# access: $var or ${var}
+echo x is $x
+
+
+
